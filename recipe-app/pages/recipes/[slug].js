b6b14abbddd1a89085874416a85985267847776c
ikfreecharles <sleektrends7@gmail.com>
2022-01-31 00:58:25 +0100

    useRouter import updated. No css added to the project

--- recipe-app/pages/recipes/[slug].js
+++ recipe-app/pages/recipes/[slug].js
@@ -6,7 +6,7 @@ import {
 } from "../../lib/sanity";
 import Image from "next/image";
 import { useState } from "react";
-import useRouter from "next/router";
+import { useRouter } from "next/router";
 
 const recipeQuery = `
 *[_type == "recipe" && slug.current == $slug][0]{
